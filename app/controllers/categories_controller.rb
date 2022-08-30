acb5585f7039427d9e8bc2a6fa879327f5c50c4f
Victor Marxen <victor.marxen@hotmail.de>
2022-08-30 17:37:39 +0100

    Adjustments

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,24 +2,15 @@ class CategoriesController < ApplicationController
   before_action :set_category, only: %i[show]
 
   def index
-    # if params[:country].present?
-    #   country = Country.find_by(name: params[:country])
-
-    #   if country
-    #     @categories = Category.joins(applications: :bookmarks)
-    #                           .where(bookmarks: { country_id: country.id })
-    #   end
-    # else
-
-    # end
     @categories = Category.all
   end
 
   def show
     if params[:country].present?
-      country = Country.find_by(name: params[:country])
+      country = Country.find_by(name: params[:country].capitalize)
+      category_id = params[:id]
 
-      @applications = Application.joins(:countries).where(countries: { id: country.id })
+      @applications = Application.joins(:countries).where(countries: { id: country&.id }, category_id:)
     end
   end
 
