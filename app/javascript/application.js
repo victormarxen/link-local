1a476a3f5d8fc0e21603b3f4d161e9a3a4ba2e0f
Conor Kelly <conorkellymail@gmail.com>
2022-09-01 12:43:56 +0100

    app listings

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,19 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+
+var coll = document.getElementsByClassName("dropdown");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
