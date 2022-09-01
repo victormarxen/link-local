74e037eb05429fa28490bc38edaa8723b96beb85
victormarxen <105793948+victormarxen@users.noreply.github.com>
2022-09-01 13:11:54 +0100

    Delete routes.rb

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root to: "pages#home"
-
-  resources :categories, only: %i[index show]
-  resources :favourites, only: %i[index create destroy]
-
-  get "my_profile", to: "users#my_profile"
-end
