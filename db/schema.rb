e6b65a0bd25aca81e016eecde27bdd20f455700a
Victor Marxen <victor.marxen@hotmail.de>
2022-09-01 13:13:36 +0100

    Links

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_31_101727) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_01_105202) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_31_101727) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "icon"
   end
 
   create_table "countries", force: :cascade do |t|
