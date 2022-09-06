d552eb2e4a39ba31990ae591afa70265599599e5
Conor Kelly <conorkellymail@gmail.com>
2022-09-06 13:53:15 +0100

    favourites page edit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,18 +3,35 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
 
-
-var coll = document.getElementsByClassName("dropdown");
+document.addEventListener('turbo:load', function() {
+  var coll = document.getElementsByClassName("dropdown");
 var i;
+if (coll.length > 0) {
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+  }
+})
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+// var coll = document.getElementsByClassName("dropdown");
+// var i;
+
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.display === "block") {
+//       content.style.display = "none";
+//     } else {
+//       content.style.display = "block";
+//     }
+//   });
+// }
